40d149e8392531e25af1dd817b1badcc5ecd7ad3
Vili Vilenius <vili.vilenius@outlook.com>
2023-08-11 00:52:52 +0300

    Limedev updated and advanced mode seems to work

--- src/numba_integrators/_API.py
+++ src/numba_integrators/_API.py
@@ -1,13 +1,19 @@
-from enum import Enum
+import enum
+from typing import Any
+from typing import Callable
+from typing import Union
 
 import numba as nb
 import numpy as np
 
 from ._aux import Float64Array
 from ._aux import nbA
+from ._aux import nbAdvanced_initial_step_signature
+from ._aux import nbAdvanced_ODE_signature
+from ._aux import nbAdvanced_step_signature
 from ._aux import nbARO
 from ._aux import nbODEtype
-
+from ._aux import nbtype
 
 # Multiply steps computed from asymptotic behaviour of errors by this.
 SAFETY = 0.9
@@ -82,7 +88,6 @@ def select_initial_step(fun, t0, y0, f0, direction, order, rtol, atol):
           else (0.01 / max(d1, d2)) ** (1 / (order + 1)))
 
     return min(100 * h0, h1)
-
 # ----------------------------------------------------------------------
 @nb.njit(nb.types.Tuple((nb.boolean,
                          nb.float64,
@@ -171,12 +176,11 @@ base_spec = (('A', nbARO(2)),
              ('error_exponent', nb.float64),
              ('h_abs', nb.float64),
              ('step_size', nb.float64),
-             ('fun', nbODEtype),
              ('atol', nbARO(1)),
              ('rtol', nbARO(1))
 )
 
-@nb.experimental.jitclass(base_spec)
+@nb.experimental.jitclass(base_spec + (('fun', nbODEtype),))
 class RK:
     """Base class for explicit Runge-Kutta methods."""
 
@@ -246,8 +250,7 @@ def convert(y0, rtol, atol) -> tuple[Float64Array, Float64Array, Float64Array]:
         atol = np.full(len(y0), atol)
 
     return y0, rtol, atol
-# ----------------------------------------------------------------------
-
+# ======================================================================
 _RK23_order = 3
 _RK23_error_estimator_order = 2
 _RK23_n_stages = 3
@@ -314,11 +317,241 @@ def RK45_direct(fun, t0, y0, t_bound, max_step, rtol, atol, first_step):
               _RK45_A, _RK45_B, _RK45_C, _RK45_E)
 # ----------------------------------------------------------------------
 def RK45(fun, t0, y0, t_bound, max_step = np.inf,
-         rtol = 1e-3, atol = 1e-6, first_step = 0.):
+         rtol = 1e-3, atol = 1e-6, first_step = 0):
 
     y0, rtol, atol = convert(y0, rtol, atol)
     return RK45_direct(fun, t0, y0, t_bound, max_step, rtol, atol, first_step)
 # ----------------------------------------------------------------------
-@nb.njit(nb.boolean(RK.class_type.instance_type), cache = True) # type: ignore
+class Solver(enum.Enum):
+    RK23 = RK23
+    RK45 = RK45
+    ALL = 'ALL'
+# ======================================================================
+# CORE FUNCTION WITH PARAMETERS AND AUXILIARY OUTPUT
+
+def _select_initial_step_advanced(fun, t0, y0, parameters, f0, direction,
+                                  order, rtol, atol) -> float:
+    """Empirically select a good initial step.
+
+    The algorithm is described in [1]_.
+
+    Parameters
+    ----------
+    fun : callable
+        Right-hand side of the system.
+    t0 : float
+        Initial value of the independent variable.
+    y0 : ndarray, shape (n,)
+        Initial value of the dependent variable.
+    f0 : ndarray, shape (n,)
+        Initial value of the derivative, i.e., ``fun(t0, y0)``.
+    direction : float
+        Integration direction.
+    order : float
+        Error estimator order. It means that the error controlled by the
+        algorithm is proportional to ``step_size ** (order + 1)`.
+    rtol : float
+        Desired relative tolerance.
+    atol : float
+        Desired absolute tolerance.
+
+    Returns
+    -------
+    h_abs : float
+        Absolute value of the suggested initial step.
+
+    References
+    ----------
+    .. [1] E. Hairer, S. P. Norsett G. Wanner, "Solving Ordinary Differential
+           Equations I: Nonstiff Problems", Sec. II.4.
+    """
+    if y0.size == 0:
+        return np.inf
+
+    scale = atol + np.abs(y0) * rtol
+    d0 = norm(y0 / scale)
+    d1 = norm(f0 / scale)
+
+    h0 = 1e-6 if d0 < 1e-5 or d1 < 1e-5 else 0.01 * d0 / d1
+
+    y1 = y0 + h0 * direction * f0
+    f1, _ = fun(t0 + h0 * direction, y1, parameters)
+    d2 = norm((f1 - f0) / scale) / h0
+
+    h1 = (max(1e-6, h0 * 1e-3) if d1 <= 1e-15 and d2 <= 1e-15
+          else (0.01 / max(d1, d2)) ** (1 / (order + 1)))
+
+    return min(100 * h0, h1)
+# ----------------------------------------------------------------------
+def _step_advanced(fun, direction, t, y, parameters, t_bound, h_abs, max_step,
+                   K, n_stages, rtol, atol, A, B, C, E, error_exponent,
+                   auxiliary):
+    if direction * (t - t_bound) >= 0: # t_bound has been reached
+        return False, t, y, auxiliary, h_abs, h_abs, K
+    t_old = t
+    y_old = y
+    min_step = 10. * np.abs(np.nextafter(t, direction * np.inf) - t)
+
+    if h_abs < min_step:
+        h_abs = min_step
+
+    while True: # := not working
+        if h_abs > max_step:
+            h_abs = max_step
+        h = h_abs * direction
+        # Updating
+        t = t_old + h
+
+        K[0] = K[-1]
+
+        if direction * (t - t_bound) >= 0:
+            t = t_bound
+            h = t - t_old
+            h_abs = np.abs(h) # There is something weird going on here
+        # RK core loop
+        for s in range(1, n_stages):
+            K[s], _ = fun(t_old + C[s] * h,
+                       y_old + np.dot(K[:s].T, A[s,:s]) * h,
+                       parameters)
+
+        y = y_old + h * np.dot(K[:-1].T, B)
+
+        K[-1], auxiliary = fun(t, y, parameters)
+
+        error_norm = norm(np.dot(K.T, E)
+                          * h
+                          / (atol + np.maximum(np.abs(y_old),
+                                              np.abs(y)) * rtol))
+
+        if error_norm < 1:
+            h_abs *= (MAX_FACTOR if error_norm == 0 else
+                            min(MAX_FACTOR,
+                                SAFETY * error_norm ** error_exponent))
+            return True, t, y, auxiliary, h_abs, h, K # Step is accepted
+        else:
+            h_abs *= max(MIN_FACTOR,
+                                SAFETY * error_norm ** error_exponent)
+            if h_abs < min_step:
+                return False, t, y, auxiliary, h_abs, h, K # Too small step size
+# ----------------------------------------------------------------------
+def Advanced(parameters_signature,
+             auxiliary_signature,
+             solver):
+
+    fun_type = nbAdvanced_ODE_signature(parameters_signature,
+                                        auxiliary_signature).as_type()
+    nb_initial_step = nb.njit(nbAdvanced_initial_step_signature(parameters_signature,
+                                                                fun_type),
+                              fastmath = True)(_select_initial_step_advanced)
+    nb_step_advanced = nb.njit(nbAdvanced_step_signature(parameters_signature,
+                                                         auxiliary_signature,
+                                                         fun_type)
+                                                         )(_step_advanced)
+    # ------------------------------------------------------------------
+    @nb.experimental.jitclass(base_spec + (('parameters', parameters_signature),
+                              ('auxiliary', auxiliary_signature),
+                              ('fun', fun_type)))
+    class RK_Advanced:
+        """Base class for explicit Runge-Kutta methods."""
+
+        def __init__(self, fun, t0, y0, parameters, t_bound, max_step, rtol, atol, first_step,
+                    order, error_estimator_order, n_stages, A, B, C, E):
+            self.order = order
+            self.error_estimator_order = error_estimator_order
+            self.n_stages = n_stages
+            self.A = A
+            self.B = B
+            self.C = C
+            self.E = E
+            self.fun = fun
+            self.t = t0
+            self.y = y0
+            self.parameters = parameters
+            self.t_bound = t_bound
+            self.K = np.zeros((self.n_stages + 1, len(y0)), dtype = self.y.dtype)
+            self.K[-1], self.auxiliary = self.fun(self.t, self.y, self.parameters)
+            self.direction = np.sign(t_bound - t0) if t_bound != t0 else 1
+            self.error_exponent = -1 / (self.error_estimator_order + 1)
+            self.atol = atol
+            self.rtol = rtol
+            self.max_step = max_step
+
+            if not first_step:
+                self.h_abs = nb_initial_step(
+                    self.fun, self.t, y0, self.parameters, self.K[-1], self.direction,
+                    self.error_estimator_order, self.rtol, self.atol)
+            else:
+                self.h_abs = first_step
+            self.step_size = self.h_abs
+        # --------------------------------------------------------------
+        def step(self) -> bool:
+            (running,
+             self.t,
+             self.y,
+             self.auxiliary,
+             self.h_abs,
+             self.step_size,
+             self.K) = nb_step_advanced(self.fun,
+                                        self.direction,
+                                        self.t,
+                                        self.y,
+                                        self.parameters,
+                                        self.t_bound,
+                                        self.h_abs,
+                                        self.max_step,
+                                        self.K,
+                                        self.n_stages,
+                                        self.rtol,
+                                        self.atol,
+                                        self.A,
+                                        self.B,
+                                        self.C,
+                                        self.E,
+                                        self.error_exponent,
+                                        self.auxiliary)
+
+            return running
+    # ------------------------------------------------------------------
+    if solver in (Solver.RK23, Solver.ALL):
+        @nb.njit(cache = False)
+        def RK23_direct_advanced(fun, t0, y0, parameters, t_bound, max_step,
+                                 rtol, atol, first_step):
+            return RK_Advanced(fun, t0, y0, parameters, t_bound, max_step,
+                               rtol, atol, first_step,
+                    _RK23_order, _RK23_error_estimator_order, _RK23_n_stages,
+                    _RK23_A, _RK23_B, _RK23_C, _RK23_E)
+        # --------------------------------------------------------------
+        def RK23_advanced(fun, t0, y0, parameters, t_bound, max_step = np.inf,
+                rtol = 1e-3, atol = 1e-6, first_step = 0.):
+
+            y0, rtol, atol = convert(y0, rtol, atol)
+            return RK23_direct_advanced(fun, t0, y0, parameters, t_bound,
+                                        max_step, rtol, atol, first_step)
+        # --------------------------------------------------------------
+        if solver == Solver.RK23:
+            return RK23_advanced
+    # ------------------------------------------------------------------
+    if solver in (Solver.RK45, Solver.ALL):
+        @nb.njit(cache = False)
+        def RK45_direct_advanced(fun, t0, y0, parameters, t_bound, max_step,
+                                 rtol, atol, first_step):
+            return RK_Advanced(fun, t0, y0, parameters, t_bound, max_step,
+                               rtol, atol, first_step,
+                    _RK45_order, _RK45_error_estimator_order, _RK45_n_stages,
+                    _RK45_A, _RK45_B, _RK45_C, _RK45_E)
+        # --------------------------------------------------------------
+        def RK45_advanced(fun, t0, y0, parameters, t_bound, max_step = np.inf,
+                rtol = 1e-3, atol = 1e-6, first_step = 0):
+
+            y0, rtol, atol = convert(y0, rtol, atol)
+            return RK45_direct_advanced(fun, t0, y0, parameters, t_bound,
+                                        max_step, rtol, atol, first_step)
+        if solver == Solver.RK45:
+            return RK45_advanced
+
+    return {Solver.RK23: RK23_advanced,
+            Solver.RK45: RK45_advanced}
+# ======================================================================
+@nb.njit # type: ignore
 def step(solver: RK) -> bool:
     return solver.step()
--- tests/unittests/test_public.py
+++ tests/unittests/test_public.py
@@ -1,5 +1,7 @@
 '''Unittests for public interface of the package.
 Classes are sorted alphabetically and related functions'''
+from typing import Callable
+
 import numba as nb
 import numba_integrators as ni
 import numpy as np
@@ -13,14 +15,14 @@ from numba_integrators._aux import Float64Array
 def function_to_integrate(t: float, y: Float64Array):
     '''Function that integrates to sine and cosine'''
     return np.array((y[1], -y[0]))
-
+t0 = 0.
 y0 = np.array((0., 1.), dtype = np.float64)
 # ======================================================================
 class Test_RK45:
     def test_initialise(self):
         x_end = 1
         solver = ni.RK45(function_to_integrate,
-                        0.,
+                        t0,
                         y0,
                         x_end,
                         rtol = 1e-8,
@@ -29,3 +31,36 @@ class Test_RK45:
             pass
         assert solver.t == x_end
         assert np.abs(solver.y[0] / np.sin(x_end) -1) < 1e-7
+# ======================================================================
+
+@nb.njit()
+def f_advanced(t, y, p):
+    dy = np.array((y[1], -y[0]))
+    a = (p[0] * t, p[1]*t)
+    return dy, a
+# ----------------------------------------------------------------------
+class Test_Advanced:
+    parameters_type = nb.types.Tuple((nb.float64, nb.float64[:]))
+    auxiliary_type = nb.types.Tuple((nb.float64, nb.float64[:]))
+    parameters = (1., np.array((1., 1.), dtype = np.float64))
+    @pytest.mark.parametrize('solver_type', (ni.RK23, ni.RK45))
+    def test_class_creation(self, solver_type):
+        Solver = ni.Advanced(nb.float64, nb.float64, solver_type)
+        assert Solver is not None
+        assert not isinstance(Solver, dict)
+    # ------------------------------------------------------------------
+    def test_all_class_creation(self):
+        Solvers = ni.Advanced(self.parameters_type, self.auxiliary_type, ni.Solver.ALL)
+        assert isinstance(Solvers, dict)
+    # ------------------------------------------------------------------
+    @pytest.mark.parametrize('solver_type', (ni.RK23, ni.RK45))
+    def test_class_use(self, solver_type):
+        Solver = ni.Advanced(self.parameters_type, self.auxiliary_type, solver_type)
+        solver = Solver(f_advanced, t0, y0, self.parameters, 1.)
+        assert isinstance(solver.auxiliary, tuple)
+        assert solver.auxiliary[0] == self.parameters[0] * solver.t
+        assert np.all(solver.auxiliary[1] == self.parameters[1] * solver.t)
+        ni.step(solver)
+        assert solver.auxiliary[0] == self.parameters[0] * solver.t
+        assert np.all(solver.auxiliary[1] == self.parameters[1] * solver.t)
+    # ------------------------------------------------------------------
