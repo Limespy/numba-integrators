00800a21979622114502a3834da3412cac19f20b
Vili Vilenius <vili.vilenius@outlook.com>
2024-07-18 00:11:30 +0300

    readme build fix

--- readme/readme.py
+++ readme/readme.py
@@ -3,33 +3,30 @@ import pathlib
 import numba_integrators as ni
 import yamdog as md
 from limedev import readme
-
-PATH_BASE = pathlib.Path(__file__).parent
-PATH_README = PATH_BASE / 'README.md'
-PATH_PYPROJECT = PATH_BASE / 'pyproject.toml'
-
 #=======================================================================
-NAME = 'Numba Integrators'
-def quick_start():
+PATH_BASE = pathlib.Path(__file__).parent
+#=======================================================================
+def quick_start() -> md.Document:
     example_text = (PATH_BASE / 'example.py').read_text()
 
     guide = md.Document([md.Heading('Example', 3),
                          md.CodeBlock(example_text, 'python')])
     return guide
 #=======================================================================
-def advanced():
+def advanced() -> md.Document:
     example_advanced = (PATH_BASE / 'example_advanced.py').read_text()
     return md.Document([md.Heading('Example of the advanced function', 3),
                          md.CodeBlock(example_advanced, 'python')])
 #=======================================================================
-def main(project_info):
+def main(pyproject: readme.PyprojectType) -> md.Document:
 
     link_numba = md.Link('https://numba.pydata.org', 'Numba',
                          'Numba organisation homepage')
     link_scipy = md.Link('https://scipy.org/', 'SciPy',
                          'SciPy organisation homepage')
+    name = pyproject['tool']['limedev']['full_name']
     semi_description = md.Document([md.Paragraph([
-        f'{NAME} is collection numerical integrators based on the ones in ',
+        f'{name} is collection numerical integrators based on the ones in ',
        link_scipy,
        '. Aim is to make them faster and much more compatible with ',
        link_numba,
@@ -37,6 +34,6 @@ def main(project_info):
     ])])
 
     return readme.make(ni, semi_description,
-                       name = NAME,
-                       quick_start = quick_start() + advanced(),
-                       pypiname = project_info['name'])
+                       name = name,
+                       abbreviation = 'ni',
+                       quick_start = quick_start() + advanced())
